9b64a24428f6bc34fba24bb5e3e77ad719cef045
Dzikra Yuhasyra <66412099+dzikrayr@users.noreply.github.com>
2026-03-13 08:52:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ st.sidebar.markdown("---")
 st.sidebar.write("Dibuat menggunakan **Streamlit** dan **TensorFlow/Keras** oleh Dzikra Yuhasyra, S.T., M.Agr. Semoga bermanfaat!")
 
 # --- HALAMAN UTAMA ---
-st.title("🏢 Classify the Damage by Dzikra Y: Sistem Deteksi Kerusakan Bangunan dan Jalan")
+st.title("🏢 Classify the Damage by Dzikra Y.: Sistem Deteksi Kerusakan Bangunan dan Jalan")
 st.write("Unggah foto bangunan dan jalan untuk menganalisis kondisinya.")
 
 # Widget untuk mengunggah gambar
